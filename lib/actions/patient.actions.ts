0a214fe8a6a002728bd49b93726bbf21dc40d1df
asep3003 <saepulloha133@gmail.com>
2025-06-05 22:59:26 +0700

    feat: register form

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,5 +1,6 @@
 import { ID, Query } from "node-appwrite";
 import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -17,5 +18,14 @@ export const createUser = async (user: CreateUserParams) => {
 
       return documents?.users[0];
     }
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+    return parseStringify(user);
+  } catch (error) {
+    console.log(error);
   }
 };
